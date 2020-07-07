9b956763774d96ebbfa1052af32b7f780eeba526
Grigori Hlopkov <grigori.hlopkov@gmail.com>
2020-07-07 21:07:14 +0200

    add heroes list and detail

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,21 +1,21 @@
 import 'package:angular/angular.dart';
-import 'package:angular_app/hero.dart';
+import 'package:angular_app/src/hero.dart';
+import 'package:angular_app/src/mock_heroes.dart';
 import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  ''',
-  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) {
+    selected = hero;
+    print("hero selected ${selected.name}");
+  }
 }
